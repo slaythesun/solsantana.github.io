90118bbc02c71227c4d3bcddc13c932a3a95bdef
Soleil Santana <soleilasantana@gmail.com>
2019-05-16 19:59:17 -0400

    Delete menu.js

--- js/menu.js
+++
@@ -1,8 +0,0 @@
-$(document).mouseup(function(e){
-   var menu = $('icon');
-   if (!menu.is(e.target) // The target of the click isn't the container.
-   && menu.has(e.target).length === 0) // Nor a child element of the container
-   {
-      menu.hide();
-   }
-});
